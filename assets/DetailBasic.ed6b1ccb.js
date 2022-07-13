431265e8e684a6c4be7023da5830c77026d0a02a
DESKTOP-V1BF6TM\One smile <1539027251@qq.com>
2022-07-13 17:15:33 +0800

    'init'

---
+++ assets/DetailBasic.ed6b1ccb.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./index.5fc53803.js";import{f as r,o as s,j as e,k as o,A as t}from"./ant-design-vue.07f5f87d.js";const d={},f=t("基础详情");var n=a(d,[["render",function(a,t){const d=r("a-card");return s(),e(d,{bordered:!1},{default:o((()=>[f])),_:1})}]]);export{n as default};
